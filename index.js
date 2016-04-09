0eea5cb7ccc9ff38327b61063acf8f7ee84ebdab
hugofestA <helixfesta@gmail.com>
2016-04-09 10:41:06 +0200

    Create first function. v1.0 HOORAYY.

---
+++ index.js
@@ -0,0 +1,36 @@
+//This code is made by hugofestA in the project REELDEEL.
+//The code is under ISC license.
+var http = require('http');
+
+var useragent = 'DiscordBot (https://github.com/hugofestA/reeldeel, 1.0)';
+
+function getGateway(){
+  http.get({
+    host: 'discordapp.com',
+    path: '/api/gateway',
+    headers: {
+      'User-Agent': useragent;
+    }
+  }, function(res) {
+    if (res.statusCode === 200) {
+      var body = [];
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {
+        body.push(chunk);
+      });
+      res.on('end', function() {
+        body = body.join('');
+        return body.url;
+      });
+    } else {
+      if (res.statusCode === 429){
+        setTimeout(function() {
+          getGateway();
+        }, parseInt(JSON.parse(res.headers).Retry-After, 10) * 1000)
+        console.log('Delay: ' + );
+      } else {
+        console.log('Received non-ok response: ' + res.statusCode);
+      }
+    }
+  });
+}
